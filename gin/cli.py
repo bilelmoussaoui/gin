d8e565f6d2e03eb2ecaae73329abf55703eafd13
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-10-05 18:50:01 +0000

    Init: generate pkgbuilds & refactor cli

--- gin/cli.py
+++ gin/cli.py
@@ -1,8 +1,29 @@
-import fire
-
+import click
 from gin.app import Gin
 
+click_path = click.Path(exists=True, readable=True, resolve_path=True)
 
-def main():
-    # Required to use console_entry from setup.py
-    fire.Fire(Gin)
+@click.group()
+@click.pass_context
+def run(ctx):
+    app = Gin()
+    ctx.obj = {
+        'app': app
+    }
+
+@run.command(help="Init the project by generating needed files to build")
+@click.argument("manifest", required=True, envvar="MANIFEST", type=click_path)
+@click.pass_context
+def init(ctx, manifest):
+    app = ctx.obj['app']
+    app.set_manifest(manifest)
+    app.prepare()
+
+@run.command(help="Build the project")
+@click.argument("bundle", required=False, envvar="BUNDLE")
+@click.argument("wixtoolset", required=False, envvar="WIXTOOLSET")
+@click.argument("manifest", required=True, envvar="MANIFEST", type=click_path)
+@click.pass_context
+def build(ctx, manifest, bundle="app.msi", wixtoolset=None):
+    app = ctx.obj['app']
+    app.build()
